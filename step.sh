ee3d8f23bad81cbfa27a4677a3317b65d45248bf
Cyril PATRICE <cpatrice@pagesjaunes.fr>
2019-02-19 16:54:53 +0100

    feat(maven): attends le démarrage du serveur

--- step.sh
+++ step.sh
@@ -4,9 +4,21 @@ set -ex
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $THIS_SCRIPT_DIR
-
-mvn mockserver:runForked 
+#curl -v "http://localhost"
+sudo mvn -q mockserver:runForked
 #config exportations
+attempt_counter=0
+max_attempts=5
+
+until $(curl --output /dev/null --silent --head --fail -X PUT "http://localhost/mockserver/status"); do
+    if [ ${attempt_counter} -eq ${max_attempts} ];then
+      echo "Max attempts reached"
+      exit 42
+    fi
+
+    attempt_counter=$(($attempt_counter+1))
+    sleep 5
+done
 
 curl -v -X PUT "http://localhost/mockserver/expectation" -d '{                                              
   "httpRequest" : {                      
